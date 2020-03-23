4c5101f2db1f17ba6f084445cd3b291a9d15c829
Your Name <lizhichange@163.com>
2020-03-23 16:56:12 +0800

    f

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysIndexController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysIndexController.java
@@ -1,36 +1,40 @@
 package com.ruoyi.web.controller.system;
 
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
 import com.ruoyi.common.config.Global;
 import com.ruoyi.common.core.controller.BaseController;
 import com.ruoyi.framework.util.ShiroUtils;
 import com.ruoyi.system.domain.SysMenu;
+import com.ruoyi.system.domain.SysShort;
 import com.ruoyi.system.domain.SysUser;
 import com.ruoyi.system.service.ISysConfigService;
 import com.ruoyi.system.service.ISysMenuService;
+import com.ruoyi.system.service.ISysShortService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 /**
  * 首页 业务处理
- * 
+ *
  * @author ruoyi
  */
 @Controller
-public class SysIndexController extends BaseController
-{
+public class SysIndexController extends BaseController {
     @Autowired
     private ISysMenuService menuService;
 
     @Autowired
     private ISysConfigService configService;
+    @Autowired
+    ISysShortService sysShortService;
 
     // 系统首页
     @GetMapping("/index")
-    public String index(ModelMap mmap)
-    {
+    public String index(ModelMap mmap) {
         // 取身份信息
         SysUser user = ShiroUtils.getSysUser();
         // 根据用户id取出菜单
@@ -41,20 +45,26 @@ public class SysIndexController extends BaseController
         mmap.put("skinName", configService.selectConfigByKey("sys.index.skinName"));
         mmap.put("copyrightYear", Global.getCopyrightYear());
         mmap.put("demoEnabled", Global.isDemoEnabled());
+
+        SysShort sysShort = new SysShort();
+        sysShort.setShortKey(user.getLoginName());
+        List<SysShort> list = sysShortService.selectSysShortList(sysShort);
+        if (!CollectionUtils.isEmpty(list)) {
+            SysShort item = list.get(0);
+            mmap.addAttribute("sysShort", item);
+        }
         return "index";
     }
 
     // 切换主题
     @GetMapping("/system/switchSkin")
-    public String switchSkin(ModelMap mmap)
-    {
+    public String switchSkin(ModelMap mmap) {
         return "skin";
     }
 
     // 系统介绍
     @GetMapping("/system/main")
-    public String main(ModelMap mmap)
-    {
+    public String main(ModelMap mmap) {
         mmap.put("version", Global.getVersion());
         return "main";
     }
