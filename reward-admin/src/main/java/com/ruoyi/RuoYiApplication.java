b10d5f9836c94fce136fd9130d3bd38012767b8e
Your Name <lizhichange@163.com>
2020-04-26 11:04:44 +0800

    commit_message

--- reward-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ reward-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -4,6 +4,7 @@ import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -14,6 +15,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableScheduling
 @NacosPropertySource(dataId = "application.yaml", autoRefreshed = true)
+@EnableEurekaClient
 public class RuoYiApplication {
     public static void main(String[] args) {
         // System.setProperty("spring.devtools.restart.enabled", "false");
