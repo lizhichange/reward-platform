675d438b01eb170a045f285d29aeca871b183d74
Your Name <lizhichange@163.com>
2020-05-08 13:31:53 +0800

    commit_message

--- reward-video-web/src/main/java/com/ruoyi/web/controller/IndexController.java
+++ reward-video-web/src/main/java/com/ruoyi/web/controller/IndexController.java
@@ -10,13 +10,9 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 @Controller
 public class IndexController extends BaseController {
-    @GetMapping()
-    public String render(@RequestParam(value = "userid", required = false) String userid, ModelMap modelmap) {
-        return "redirect:/video";
-    }
 
     @GetMapping("/index")
     public String index(@RequestParam(value = "userid", required = false) String userid, ModelMap modelmap) {
-        return render(userid, modelmap);
+        return "redirect:/video";
     }
 }
