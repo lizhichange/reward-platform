fe074c79f5bc45d8e83130b62e1595705a4e9599
Your Name <lizhichange@163.com>
2020-05-08 13:13:54 +0800

    commit_message

--- reward-video-web/src/main/java/com/ruoyi/web/controller/IndexController.java
+++ reward-video-web/src/main/java/com/ruoyi/web/controller/IndexController.java
@@ -1,11 +1,16 @@
 package com.ruoyi.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @author sunflower
  */
 @Controller
-public class IndexController {
+public class IndexController extends BaseController {
 
+    public String index(@RequestParam(value = "userid", required = false) String userid, ModelMap modelmap) {
+        return "redirect:/video";
+    }
 }
