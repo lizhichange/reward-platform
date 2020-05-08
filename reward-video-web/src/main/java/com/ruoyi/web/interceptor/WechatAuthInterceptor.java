b8160da68c44c37a397aa303b7e22c96dd97a34a
Your Name <lizhichange@163.com>
2020-05-08 21:22:54 +0800

    commit_message

--- reward-video-web/src/main/java/com/ruoyi/web/interceptor/WechatAuthInterceptor.java
+++ reward-video-web/src/main/java/com/ruoyi/web/interceptor/WechatAuthInterceptor.java
@@ -14,6 +14,7 @@ import org.near.toolkit.context.SessionContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.context.request.RequestAttributes;
@@ -49,6 +50,7 @@ public class WechatAuthInterceptor extends HandlerInterceptorAdapter {
     @Autowired
     AppConfig appConfig;
 
+    @Lazy
     @Autowired
     TsFeign tsFeign;
 
