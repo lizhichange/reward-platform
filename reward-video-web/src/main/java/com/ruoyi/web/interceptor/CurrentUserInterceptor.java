b96aedf9b688f969c50380738577c6a6e1013225
Your Name <lizhichange@163.com>
2020-05-17 18:56:30 +0800

    commit_message

--- reward-video-web/src/main/java/com/ruoyi/web/interceptor/CurrentUserInterceptor.java
+++ reward-video-web/src/main/java/com/ruoyi/web/interceptor/CurrentUserInterceptor.java
@@ -1,11 +1,13 @@
 package com.ruoyi.web.interceptor;
 
 
+import com.ruoyi.web.config.RedisUtil;
 import com.ruoyi.web.model.Users;
 import com.ruoyi.web.security.SecurityUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
@@ -23,11 +25,14 @@ import javax.servlet.http.HttpServletResponse;
 public class CurrentUserInterceptor extends HandlerInterceptorAdapter {
     private final static Logger LOGGER = LoggerFactory.getLogger(CurrentUserInterceptor.class);
 
+    @Autowired
+    RedisUtil redisUtil;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         Users currentUser = SecurityUtil.getCurrentUser();
         request.setAttribute("user", currentUser);
+        redisUtil.set("user", currentUser);
         return super.preHandle(request, response, handler);
 
 
