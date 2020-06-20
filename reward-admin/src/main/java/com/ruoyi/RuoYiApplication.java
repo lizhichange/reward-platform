e230a10020693edbe0ce3ca565841b24b50e73d8
Your Name <lizhichange@163.com>
2020-06-20 15:04:49 +0800

    x

--- reward-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ reward-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -8,10 +8,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import javax.annotation.PostConstruct;
 import java.util.Date;
 import java.util.List;
 
@@ -24,16 +24,15 @@ import java.util.List;
 @EnableScheduling
 @EnableEurekaClient
 @EnableDiscoveryClient
+@RefreshScope
 public class RuoYiApplication {
     public static void main(String[] args) {
-        // System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(RuoYiApplication.class, args);
     }
 
     @Autowired
     ShipinMapper shipinMapper;
 
-    @PostConstruct
     void testOne() {
         List<Shipin> list = shipinMapper.selectByExample(new ShipinExample());
         for (Shipin shipin : list) {
