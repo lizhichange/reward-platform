f689407bd4a7434edef9f0e553d44e3866340475
Your Name <lizhichange@163.com>
2020-06-20 15:54:05 +0800

    commit_message

--- reward-video-web/bin/update.sh
+++ reward-video-web/bin/update.sh
@@ -1,22 +1,18 @@
-
-
 path=
 
 project=reward-platform
-if [ ! -n "$1" ];then
+if [ ! -n "$1" ]; then
   echo "NULL"
 else
- path=$1
+  path=$1
 fi
 
-
-cd  //home/fulihui/code/${project}${path}
+cd //home/fulihui/code/${project}${path}
 
 git pull
 
 mvn clean package -DskipTests
 
+scp /home/fulihui/code/${project}${path}/reward-video-web/config/* /home/fulihui/micro-sevr/${project}/reward-video-web/config/.
 
-scp  /home/fulihui/code/${project}${path}/reward-video-web/config/*  /home/fulihui/micro-svr/${project}/reward-video-web/config/.
-
-scp  /home/fulihui/code/${project}${path}/reward-video-web/target/reward-video-web.jar  /home/fulihui/micro-svr/${project}/reward-video-web/.
+scp /home/fulihui/code/${project}${path}/reward-video-web/target/reward-video-web.jar /home/fulihui/micro-sevr/${project}/reward-video-web/.
