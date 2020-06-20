f100312023470c715be20ab41af1199788bbc1f1
Your Name <lizhichange@163.com>
2020-06-20 16:00:25 +0800

    commit_message

--- reward-mp/bin/update.sh
+++ reward-mp/bin/update.sh
@@ -17,6 +17,6 @@ git pull
 mvn clean package -DskipTests
 
 
-scp  /home/fulihui/code/${project}${path}/reward-mp/config/*  /home/fulihui/micro-svr/${project}/reward-mp/config/.
+scp  /home/fulihui/code/${project}${path}/reward-mp/config/*  /home/fulihui/micro-sevr/${project}/reward-mp/config/.
 
-scp  /home/fulihui/code/${project}${path}/reward-mp/target/reward-mp.jar  /home/fulihui/micro-svr/${project}/reward-mp/.
+scp  /home/fulihui/code/${project}${path}/reward-mp/target/reward-mp.jar  /home/fulihui/micro-sevr/${project}/reward-mp/.
