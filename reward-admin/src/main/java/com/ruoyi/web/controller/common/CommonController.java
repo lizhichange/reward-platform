782db08a7c942d7b6ebe55a651f9813c4dda4820
Your Name <lizhichange@163.com>
2020-06-23 13:17:59 +0800

    commit_message

--- reward-admin/src/main/java/com/ruoyi/web/controller/common/CommonController.java
+++ reward-admin/src/main/java/com/ruoyi/web/controller/common/CommonController.java
@@ -10,6 +10,7 @@ import com.ruoyi.common.utils.file.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,6 +32,14 @@ public class CommonController {
 
     @Autowired
     private ServerConfig serverConfig;
+
+    @Value("${didispace.title:}")
+    private String title;
+
+    @GetMapping("common/test")
+    public String hello() {
+        return title;
+    }
 
     /**
      * 通用下载请求
