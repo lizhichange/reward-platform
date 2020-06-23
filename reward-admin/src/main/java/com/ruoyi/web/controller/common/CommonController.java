82177b230b84bb109b2549610daa8cb805c34e6b
Your Name <lizhichange@163.com>
2020-06-23 14:07:29 +0800

    commit_message

--- reward-admin/src/main/java/com/ruoyi/web/controller/common/CommonController.java
+++ reward-admin/src/main/java/com/ruoyi/web/controller/common/CommonController.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,6 +28,8 @@ import javax.servlet.http.HttpServletResponse;
  * @author ruoyi
  */
 @Controller
+@RefreshScope
+
 public class CommonController {
     private static final Logger log = LoggerFactory.getLogger(CommonController.class);
 
