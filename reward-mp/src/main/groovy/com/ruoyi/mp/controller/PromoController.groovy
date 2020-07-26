000886fed54a132f957b4c3c388b3de5146aef7c
Your Name <lizhichange@163.com>
2020-07-26 19:06:37 +0800

    commit_message

--- reward-mp/src/main/groovy/com/ruoyi/mp/controller/PromoController.groovy
+++ reward-mp/src/main/groovy/com/ruoyi/mp/controller/PromoController.groovy
@@ -1,11 +1,12 @@
 package com.ruoyi.mp.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 class PromoController {
-    @RequestMapping("promo")
+    @GetMapping("/book")
     String render() {
         return "promo"
     }
