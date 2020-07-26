acc5d3ea45984e3276fe68dd8c00340bf03240d2
Your Name <lizhichange@163.com>
2020-07-26 19:12:20 +0800

    commit_message

--- reward-admin/src/main/groovy/com/ruoyi/web/controller/XXXController.groovy
+++ reward-admin/src/main/groovy/com/ruoyi/web/controller/XXXController.groovy
@@ -4,10 +4,7 @@ package com.ruoyi.web.controller
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 
-@Controller
+
 class XXXController {
-    @RequestMapping(value = "xxx")
-    def xxx() {
-        return "";
-    }
+
 }
