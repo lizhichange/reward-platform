4127b5a9c8d0decbc8bbe4dbfc11d897e138ccd3
lizhichange@163.com <Lizhi1234>
2020-12-26 21:20:48 +0800

    commit_message

--- reward-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ reward-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -29,36 +29,4 @@ public class RuoYiApplication {
     public static void main(String[] args) {
         SpringApplication.run(RuoYiApplication.class, args);
     }
-
-    @Autowired
-    VideoMapper videoMapper;
-
-    void testOne() {
-        List<Video> list = videoMapper.selectByExample(new VideoExample());
-        for (Video video : list) {
-            for (int i = 0; i < 100; i++) {
-                Video newShi = new Video();
-                newShi.setMoney(video.getMoney());
-                newShi.setSj(video.getSj());
-                newShi.setCs(video.getCs());
-                newShi.setUrl(video.getUrl());
-                newShi.setUserId(video.getUserId());
-                newShi.setName(video.getName());
-                newShi.setVideoUrl(video.getVideoUrl());
-                newShi.setDuration(video.getDuration());
-                newShi.setLx(video.getLx());
-                newShi.setStatus(video.getStatus());
-                newShi.setLogo(video.getLogo());
-                newShi.setIsLei(video.getIsLei());
-                newShi.setIsAllow(video.getIsAllow());
-                newShi.setClick(video.getClick());
-                newShi.setIsPreview(video.getIsPreview());
-                newShi.setShortUrl(video.getShortUrl());
-                newShi.setCategoryId(video.getCategoryId());
-                newShi.setCreateTime(new Date());
-                videoMapper.insertSelective(newShi);
-            }
-        }
-
-    }
 }
