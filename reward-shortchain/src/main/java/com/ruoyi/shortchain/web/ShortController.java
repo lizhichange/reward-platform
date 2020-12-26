ad9b6ec059d6eac3364f18d056b01dcc80471390
lizhichange@163.com <Lizhi1234>
2020-12-26 23:18:30 +0800

    commit_message

--- reward-shortchain/src/main/java/com/ruoyi/shortchain/web/ShortController.java
+++ reward-shortchain/src/main/java/com/ruoyi/shortchain/web/ShortController.java
@@ -1,14 +1,18 @@
 package com.ruoyi.shortchain.web;
 
+import com.ruoyi.shortchain.client.SysShortFacadeClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ShortController {
 
-    @RequestMapping("/check")
-    public Object userList(String url) {
-        return checkUrlService.check(url);
+    @Autowired
+    SysShortFacadeClient sysShortFacadeClient;
+    @RequestMapping("/short")
+    public Object shortLink(Long id) {
+        return sysShortFacadeClient.selectSysShortById(id);
     }
 
 }
