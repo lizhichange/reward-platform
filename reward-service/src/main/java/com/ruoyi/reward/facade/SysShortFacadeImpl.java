64874aed99c6a29ded655d09da386b77b408ab29
lizhichange@163.com <Lizhi1234>
2020-12-26 23:16:38 +0800

    commit_message

--- reward-service/src/main/java/com/ruoyi/reward/facade/SysShortFacadeImpl.java
+++ reward-service/src/main/java/com/ruoyi/reward/facade/SysShortFacadeImpl.java
@@ -8,7 +8,7 @@ import com.ruoyi.reward.service.SysShortService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.CollectionUtils;
-
+import com.ruoyi.reward.domain.SysShort;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
