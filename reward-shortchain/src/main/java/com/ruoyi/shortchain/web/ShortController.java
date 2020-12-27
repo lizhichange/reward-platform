efcb5a93a236898caa384a7b0d1703dfc6b28133
lizhichange@163.com <Lizhi1234>
2020-12-27 13:56:09 +0800

    commit_message

--- reward-shortchain/src/main/java/com/ruoyi/shortchain/web/ShortController.java
+++ reward-shortchain/src/main/java/com/ruoyi/shortchain/web/ShortController.java
@@ -1,18 +1,41 @@
 package com.ruoyi.shortchain.web;
 
+import cn.hutool.core.util.RandomUtil;
+import com.ruoyi.reward.facade.dto.SysShortDTO;
 import com.ruoyi.shortchain.client.SysShortFacadeClient;
+import org.near.toolkit.model.AjaxResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
+/**
+ * @author wahaha
+ */
 @RestController
+
 public class ShortController {
 
     @Autowired
     SysShortFacadeClient sysShortFacadeClient;
+
     @RequestMapping("/short")
     public Object shortLink(Long id) {
         return sysShortFacadeClient.selectSysShortById(id);
     }
+
+    @PostMapping("/generate")
+    public AjaxResult generate(String url) {
+        SysShortDTO sysShortDTO = new SysShortDTO();
+        sysShortDTO.setLongUrl(url);
+        sysShortDTO.setShortKey(String.valueOf(RandomUtil.randomNumber()));
+
+        sysShortFacadeClient.insertSysShort(sysShortDTO);
+        return AjaxResult.success();
+    }
+
 
 }
